5e5cd1ffc8c0467cec13ceaafc0ace197609849a
imgurbot12 <imgurbot12@gmail.com>
2023-10-26 20:36:23 -0700

    chore: upgrade version for latest fix

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
     name='pypub3',
-    version='2.0.3',
+    version='2.0.4',
     license='MIT',
     author='Andrew Scott',
     author_email='imgurbot12@gmail.com',
